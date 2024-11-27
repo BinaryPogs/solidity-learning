9522e474186d1741c73cb00cdbca48e3f4c806a0
Eddie <eddie.zeng95@gmail.com>
2024-11-27 23:33:03 +1100

    Deploy first smart contract and retrieve contract using js + queries

---
+++ contracts/Box.sol
@@ -0,0 +1,27 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private _value;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 value);
+
+    // When box is deployed, set ownable to deployer's address
+    constructor() Ownable(msg.sender) {}
+
+    // Stores a new value in the contract, only owner can call this
+    function store(uint256 value) public onlyOwner {
+        _value = value;
+        emit ValueChanged(value);
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
